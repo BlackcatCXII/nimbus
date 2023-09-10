cd0108c285959f49f87876595e07a6fdb401fc6c
Blackcat <75856980+BlackcatCXII@users.noreply.github.com>
2023-09-09 22:03:29 -0300

    TWITTER FIX

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@ export const SITE_NAME = "RamoAR";
 export const SITE_TITLE = "Blackcat | A personal blog For Home Work";
 export const SITE_DESCRIPTION =
 "Blackcat | A personal blog For Home Work";
-export const CLASS_HANDLE = "https://tinyurl.com/realidad-aumentada-uss";
+export const TWITTER_HANDLE = "https://tinyurl.com/realidad-aumentada-uss";
 export const GITHUB_HANDLE = "https://github.com/BlackcatCXII";
 export const CLOUDINARY_NAME = "example";
